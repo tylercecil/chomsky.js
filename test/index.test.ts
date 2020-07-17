dbb1a177d684d12a4fc9327bbb500c5efeaf1060
Duncan Gibbs <duncgibbs@gmail.com>
2020-07-17 15:18:08 -0500

    Sets up the initial project structure (#6)
    Sets up the initial project structure
    
    Co-authored-by: Tyler Cecil <tyler.i.cecil@gmail.com>

---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+describe('overall test suite component', () => {
+    test('specific test', () => {
+        expect(true);
+    });
+});
---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
