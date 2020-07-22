7ab97c807fe4f0ac961648f29486ce4e2a84bbb7
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-07-22 14:01:09 -0700

    Adds skeleton LL(1) recursive descent parser.

--- src/parser.ts
+++ src/parser.ts
@@ -3,13 +3,124 @@ interface Token {
   value?: string;
 }
 
+interface Tree {
+  nType: { name: string; sup: string; sub: string };
+  isCollapsed: boolean;
+  nClass: string;
+  children: [];
+  data: string;
+}
+
+/**
+ * Parses a given string, returning a Tree object.
+ *
+ * @param input
+ * @returns Resulting tree.
+ */
+function parse(input: string) {
+  const toks = tokenize(input);
+  let tok_peek = toks.next().value;
+
+  function accept(tokenType: string) {
+    accept_ws();
+    return accept_ws(tokenType);
+  }
+
+  function accept_ws(tokenType = 'WHITESPACE') {
+    let tok = null;
+    if (tok_peek.type === tokenType) {
+      tok = tok_peek;
+      tok_peek = toks.next().value;
+    }
+    return tok;
+  }
+
+  function expect(tokenType: string) {
+    const tok = accept(tokenType);
+    if (!tok) {
+      throw 'Unexpected token!: ' + tok_peek.type;
+    }
+    return tok;
+  }
+
+  function node() {
+    expect('[');
+    accept_ws();
+    if (tok_peek.type == 'WORD') {
+      nodeType();
+      while (accept('.')) {
+        nodeType();
+      }
+
+      // + '' is to make typescript be quite...
+      switch (tok_peek.type + '') {
+        case '[':
+          nodeList();
+          break;
+        case '*':
+        case '.':
+        case '/':
+        case '_':
+        case '^':
+        case 'WORD':
+          nodeData();
+          break;
+      }
+    }
+
+    expect(']');
+  }
+
+  function nodeType() {
+    expect('WORD');
+
+    if (accept('_')) {
+      expect('WORD');
+      if (accept('^')) {
+        expect('WORD');
+      }
+    } else if (accept('^')) {
+      expect('WORD');
+      if (accept('_')) {
+        expect('WORD');
+      }
+    }
+  }
+
+  function nodeList() {
+    accept_ws();
+    while (tok_peek.type === '[') {
+      node();
+      accept_ws();
+    }
+  }
+
+  function nodeData() {
+    if (accept('/')) {
+      // Not empty
+    } else {
+      accept('*');
+      expect('WORD');
+      while (
+        tok_peek.type != '[' &&
+        tok_peek.type != ']' &&
+        tok_peek.type != 'EOF'
+      ) {
+        tok_peek = toks.next().value;
+      }
+    }
+  }
+
+  return undefined;
+}
+
 /**
  * Returns generator, producing tokens from an input string to be parsed.
  *
  * @param input
  * @returns Generator of tokens.
  */
-function* tokenizer(input: String): Generator<Token, Token, void> {
+function* tokenize(input: string): Generator<Token, Token, void> {
   const whitespace = /\s/;
   const symbols = /[.*/_^[\]]/;
   let cur = 0;
@@ -42,4 +153,4 @@ function* tokenizer(input: String): Generator<Token, Token, void> {
   return { type: 'EOF' };
 }
 
-export { tokenizer };
+export { tokenize };
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,9 +1,9 @@
-import { tokenizer } from '../src/parser.ts';
+import { tokenize } from '../src/parser.ts';
 
 describe('lexer and parser tests', () => {
   test('lexer tokenizes symbols', () => {
     const symbols = ['.', '*', '/', '_', '^', '[', ']'];
-    const toks = tokenizer(symbols.join(''));
+    const toks = tokenize(symbols.join(''));
 
     symbols.forEach((sym) => {
       expect(toks.next().value.type).toBe(sym);
@@ -14,7 +14,7 @@ describe('lexer and parser tests', () => {
 
   test('lexer tokenizes whitespace as whole tokens', () => {
     const spaces = [' ', '   ', '\n\n', '\t\n ', '\t\n   '];
-    const toks = tokenizer(spaces.join('.') + '.');
+    const toks = tokenize(spaces.join('.') + '.');
 
     spaces.forEach((space) => {
       let tok = toks.next().value;
@@ -29,7 +29,7 @@ describe('lexer and parser tests', () => {
 
   test('lexer tokenizes words as whole tokens', () => {
     const words = ['a', 'aa', 'aaa'];
-    const toks = tokenizer(words.join(' ') + ' ');
+    const toks = tokenize(words.join(' ') + ' ');
 
     words.forEach((word) => {
       let tok = toks.next().value;
@@ -45,7 +45,7 @@ describe('lexer and parser tests', () => {
   test('lexer breaks words on whitespace and symbols', () => {
     const breakers = ['.', '*', '/', '_', '^', '[', ']', ' ', '\t', '\n'];
     const word = 'aaa';
-    const toks = tokenizer(breakers.join(word) + word);
+    const toks = tokenize(breakers.join(word) + word);
 
     breakers.slice(0, 7).forEach((breaker) => {
       let tok = toks.next().value;
