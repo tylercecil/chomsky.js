147e7886d3e0e811cd052dbfdd523fcc158b8cdc
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-07-23 18:56:05 -0700

    Adds a skeleton recursive decent parser. (#12)

--- src/parser.ts
+++ src/parser.ts
@@ -16,12 +16,118 @@ type TokenKind =
   | 'EOF';
 
 /**
+ * Parses a given string, returning a Tree object.
+ *
+ * @param input
+ * @returns Resulting tree.
+ */
+function parse(input: string) {
+  const toks = tokenize(input);
+  let tok_peek = toks.next().value;
+
+  const root = node();
+  accept_ws();
+  expect('EOF');
+  return root;
+
+  function accept(tokenType: string) {
+    accept_ws();
+    return accept_ws(tokenType);
+  }
+
+  function accept_ws(tokenType = 'WHITESPACE') {
+    let tok = null;
+    if (tok_peek.kind === tokenType) {
+      tok = tok_peek;
+      tok_peek = toks.next().value;
+    }
+    return tok;
+  }
+
+  function expect(tokenType: string) {
+    const tok = accept(tokenType);
+    if (!tok) {
+      throw 'Unexpected token!: ' + tok_peek.kind;
+    }
+    return tok;
+  }
+
+  function node() {
+    expect('[');
+    accept_ws();
+    if (tok_peek.kind == 'WORD') {
+      nodeType();
+      while (accept('.')) {
+        nodeType();
+      }
+
+      // + '' is to make typescript be quite...
+      switch (tok_peek.kind as TokenKind) {
+        case '[':
+          nodeList();
+          break;
+        case '*':
+        case '.':
+        case '/':
+        case '_':
+        case '^':
+        case 'WORD':
+          nodeData();
+          break;
+      }
+    }
+
+    expect(']');
+  }
+
+  function nodeType() {
+    expect('WORD');
+
+    if (accept('_')) {
+      expect('WORD');
+      if (accept('^')) {
+        expect('WORD');
+      }
+    } else if (accept('^')) {
+      expect('WORD');
+      if (accept('_')) {
+        expect('WORD');
+      }
+    }
+  }
+
+  function nodeList() {
+    accept_ws();
+    while (tok_peek.kind === '[') {
+      node();
+      accept_ws();
+    }
+  }
+
+  function nodeData() {
+    if (accept('/')) {
+      // Not empty
+    } else {
+      accept('*');
+      expect('WORD');
+      while (
+        tok_peek.kind != '[' &&
+        tok_peek.kind != ']' &&
+        tok_peek.kind != 'EOF'
+      ) {
+        tok_peek = toks.next().value;
+      }
+    }
+  }
+}
+
+/**
  * Returns generator, producing tokens from an input string to be parsed.
  *
  * @param input
  * @returns Generator of tokens.
  */
-function* tokenizer(input: string): Generator<Token, Token, void> {
+function* tokenize(input: string): Generator<Token, Token, void> {
   const whitespace = /\s/;
   const symbols = /[.*/_^[\]]/;
   let cur = 0;
@@ -54,4 +160,4 @@ function* tokenizer(input: string): Generator<Token, Token, void> {
   return { kind: 'EOF' as const };
 }
 
-export { tokenizer };
+export { tokenize, parse };
