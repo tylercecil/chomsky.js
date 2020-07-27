3e296d6417c992b5578a8f2fbf550175fa8a18db
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-07-27 13:58:57 -0700

    Establishes early rendering functionality.
    Alpha draft of rendering trees, including:
    - Adding an SVG to a given div
    - Providing svg with viewBox and max-width
    - Drawing nodes and links
    
    Nodes currently contain:
    - an ellipse background
    - Text for nodeType (including superscript and subscripts)
    - Text for data
    
    No support provided for
    - collapsed trees
    - flextree
    - custome styling
    
    Noted issues: Currently there is no way of knowing the order of subscripts and
    superscripts. This is an issue with the underlying data model. Will file an
    issue.
    
    Noted issues: There is pretty rampant overflow issues. Not going to address
    until I've switched over to fextree.

--- src/render.ts
+++ src/render.ts
@@ -2,18 +2,35 @@ import { Tree } from './tree';
 import { Selection } from 'd3-selection';
 import { hierarchy, tree as d3tree, HierarchyPointNode } from 'd3-hierarchy';
 
-// TODO: Make a configuration object, which the user may update.
-const nodeSize = 0.2;
-const linkWidth = 0.01;
-const margin = { x: 1.5 * nodeSize, y: 1.2 * nodeSize };
-const maxScale = 150;
-
 type Div = Selection<HTMLDivElement, string, HTMLElement, undefined>;
 type SVG = Selection<SVGSVGElement, string, HTMLElement, undefined>;
 type TreeData = HierarchyPointNode<Tree>;
 
 /**
+ * Bundles together settings and options used for rendering a tree.
+ *
+ * Nodes are assumed to have size 1x1 in viewBox coordinates, so all numbers
+ * can be thought of as percentage of node radius.
+ *
+ * TODO: This should eventually be in its own file, and expanded upon.
+ */
+const config = {
+  nodeSize: 0.2,
+  linkWidth: 0.01,
+  margin: {
+    x: 1.5 * 0.2,
+    y: 1.2 * 0.2,
+  },
+  maxScale: 150,
+};
+
+/**
  * Renders `tree` into an SVG, and then sets the body of `div` to that SVG.
+ *
+ * Note: This will clear the existing body of div first.
+ *
+ * TODO: `render` currently ignores collapsed trees.
+ * TODO: `render` should use d3-flextree, to support varying node sizes.
  *
  * @param {Tree} tree Tree to be rendered into SVG.
  * @param {Selection} div `<div>` to be associated with `tree`.
@@ -31,19 +48,39 @@ export function render(tree: Tree, div: Div) {
   styleTree(svg);
 }
 
-function styleTree(svg: SVG) {
-  svg.selectAll('g.nodes g.node ellipse').style('fill', 'white');
-  svg
-    .selectAll('g.links line')
-    .style('stroke', 'black')
-    .style('stroke-width', linkWidth);
+/**
+ * Initializes an SVG object inside the given div. This SVG will only have two
+ * child nodes: `<g class="links">` and `<g class="nodes">`.
+ *
+ * Because rendering takes place inside a `viewBox`, a reasonable max-width is
+ * provided, based on `config.maxScale`.
+ *
+ * @param div parent div element.
+ * @param root data to be rendered in SVG. Provided to generate `viewBox`.
+ * @return newly created SVG child of `div`.
+ */
+function makeSVG(div: Div, root: TreeData) {
+  const vb = viewBox(root);
+  const svg = div
+    .append('svg')
+    .attr('viewBox', vb.join(' '))
+    .style('max-width', vb[2] * config.maxScale)
+    .style('display', 'block')
+    .style('margin', 'auto');
+  svg.append('g').classed('links', true);
+  svg.append('g').classed('nodes', true);
+
+  return svg;
 }
 
+/**
+ * Returns the needed viewBox dimensions of an SVG which should contain `root`,
+ * providing a margin based on `config.margin`.
+ *
+ * @param root data being rendered.
+ * @return calculates necessary `[minX, minY, maxX, maxY]`
+ */
 function viewBox(root: TreeData) {
-  // By default, the layout designates a node as being 1x1, so all calculations
-  // here may be done as a percentage of that node size. The viewbox will
-  // ultimately contain the entire tree.
-
   const max = { x: -Infinity, y: -Infinity };
   const min = { x: Infinity, y: Infinity };
   root.each((d) => {
@@ -54,27 +91,16 @@ function viewBox(root: TreeData) {
   });
 
   return [
-    min.x - margin.x,
-    min.y - margin.y,
-    max.x - min.x + 2 * margin.x,
-    max.y - min.y + 2 * margin.y,
+    min.x - config.margin.x,
+    min.y - config.margin.y,
+    max.x - min.x + 2 * config.margin.x,
+    max.y - min.y + 2 * config.margin.y,
   ];
 }
 
-function makeSVG(div: Div, root: TreeData) {
-  const vb = viewBox(root);
-  const svg = div
-    .append('svg')
-    .attr('viewBox', vb.join(' '))
-    .style('max-width', vb[2] * maxScale)
-    .style('display', 'block')
-    .style('margin', 'auto');
-  svg.append('g').classed('links', true);
-  svg.append('g').classed('nodes', true);
-
-  return svg;
-}
-
+/**
+ * Attach all nodes from `root` to `svg`'s `<g class="nodes">` child.
+ */
 function makeNodes(svg: SVG, root: TreeData) {
   svg
     .select('g.nodes')
@@ -86,28 +112,47 @@ function makeNodes(svg: SVG, root: TreeData) {
         .append('ellipse')
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y)
-        .attr('ry', nodeSize)
-        .attr('rx', nodeSize);
-      node
+        .attr('ry', config.nodeSize)
+        .attr('rx', config.nodeSize);
+      const nodeType = node
         .append('text')
         .text((d) => d.data.nodeType?.name ?? '')
         .attr('x', (d) => d.x)
-        .attr('y', (d) => d.y - 0.1 * nodeSize)
-        .attr('font-size', nodeSize)
+        .attr('y', (d) => d.y)
+        .attr('font-size', config.nodeSize)
         .attr('dominant-baseline', 'middle')
         .attr('text-anchor', 'middle');
+      nodeType
+        .filter((d) => d.data.nodeType?.sub != '')
+        .append('tspan')
+        .text((d) => d.data.nodeType!.sub)
+        .attr('font-size', config.nodeSize * 0.9)
+        .attr('baseline-shift', 'sub');
+      nodeType
+        .filter((d) => d.data.nodeType?.sup != '')
+        .append('tspan')
+        .text((d) => d.data.nodeType!.sup)
+        .attr('font-size', config.nodeSize * 0.9)
+        .attr('baseline-shift', 'super');
+
       node
+        .filter((d) => d.data.leaf?.data != null)
         .append('text')
-        .text((d) => d.data.leaf?.data ?? '')
+        .text((d) => d.data.leaf!.data)
         .attr('x', (d) => d.x)
-        .attr('y', (d) => d.y + nodeSize)
-        .attr('font-size', nodeSize * 0.9)
+        .attr('y', (d) => d.y)
+        .attr('dx', 0)
+        .attr('dy', '1.5em')
+        .attr('font-size', config.nodeSize * 0.9)
         .attr('dominant-baseline', 'middle')
         .attr('text-anchor', 'middle');
       return node;
     });
 }
 
+/**
+ * Attach all edges from `root` to `svg`'s `<g class="links">` child.
+ */
 function makeLinks(svg: SVG, root: TreeData) {
   svg
     .select('g.links')
@@ -120,4 +165,17 @@ function makeLinks(svg: SVG, root: TreeData) {
     .attr('y1', (d) => d.source.y)
     .attr('x2', (d) => d.target.x)
     .attr('y2', (d) => d.target.y);
+}
+
+/**
+ * Updates tree with any needed styling.
+ *
+ * TODO this style should eventually come from `config`
+ */
+function styleTree(svg: SVG) {
+  svg.selectAll('g.nodes g.node ellipse').style('fill', 'white');
+  svg
+    .selectAll('g.links line')
+    .style('stroke', 'black')
+    .style('stroke-width', config.linkWidth);
 }
