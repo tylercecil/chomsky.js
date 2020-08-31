61b80cbe88c3eb112f86d22c02b5a414659b8d80
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-08-30 19:46:41 -0700

    Improved render code. (#33)
    * Adds geometry functions in a single dictionary.
    
    * Cleans up render imports.
    
    * Removes node SVG from the data. This is contained elsewhere in D3.
    
    * Attempts to cap font size. Only works if parent font size is listed in px (not percent, em, or another unit.
    
    * Removes the TreeMargin, which can simply be handeld by the outer CSS.
    
    * Lifts all render functions into a single closure.
    
    This will let config be a function-local entity, meaning that for each call to
    render, a unique configuration may be used.
    
    * Organizational and documentation changes.

--- src/render.ts
+++ src/render.ts
@@ -1,312 +1,287 @@
 import { Tree } from './tree';
-import { select, Selection, BaseType } from 'd3-selection';
+import { Selection } from 'd3-selection';
 import { flextree, FlexHierarchy } from 'd3-flextree';
 
 type Div = Selection<HTMLDivElement, string, HTMLElement, undefined>;
 type SVG = Selection<SVGSVGElement, string, HTMLElement, undefined>;
-
-/**
- * TreeData is a Tree with one addition: it's bound SVG element.
- * This allows for easy updating after the SVG is made (which is useful, because
- * node size is based on the size of the SVG element).
- */
-interface TreeData extends Tree {
+interface TreeWithSize extends Tree {
   size: [number, number];
-  nodeG: SVGGElement;
 }
-type Hierarchy = FlexHierarchy<TreeData>;
+type Hierarchy = FlexHierarchy<TreeWithSize>;
 
-/**
- * Bundles together settings and options used for rendering a tree.
- *
- * Nodes are assumed to have size 1x1 in viewBox coordinates, so all numbers
- * can be thought of as percentage of node radius.
- *
- * TODO: This should eventually be in its own file, and expanded upon.
- */
-const config = {
+const defaultSpacing = {
   /**
-   * Individual node padding (padding.x defines left and right padding).
+   * Individual node padding (always centered).
    */
   padding: {
     x: 0.25,
     y: 0.25,
   },
   /**
-   * Individual node margin (margin.x defines left and right margin).
+   * Individual node margin (always centered).
    */
   margin: {
     x: 0.125,
     y: 1,
   },
   /**
-   * Extra space to be placed between lines of text. 0 causes BBoxes to totally
-   * overlap. Given in em units.
+   * Line Spacing within a node.
    */
   lineSpacing: 1.2,
-  /**
-   * Stroke width of all figures.
-   */
-  strokeWidth: 0.05,
-  /**
-   * Margin for the entire svg (treeMargin.x defines left and right margin)
-   */
-  treeMargin: {
-    x: 1,
-    y: 1,
-  },
-  maxScale: 40,
 };
 
-// Flextree has a confusing x/y/top/bottom/left/right scheme. To avoid mistakes,
-// I'm making my own.
-// I need to fork flextree... it's kinda a dead project
-const cx = (d: Hierarchy) => d.x;
-const cy = (d: Hierarchy) => d.top + d.ySize / 2;
-const top = (d: Hierarchy) => d.top;
-const bottom = (d: Hierarchy) => d.bottom;
-const left = (d: Hierarchy) => d.left;
-const right = (d: Hierarchy) => d.right;
+const defaultStyle = {
+  strokeWidth: 0.05,
+};
 
 /**
- * Renders `tree` into an SVG, and then sets the body of `div` to that SVG.
+ * Appends an SVG to `div`, in which `tree` will be rendered as a visual syntax
+ * tree.
  *
- * Note: This will clear the existing body of div first.
- *
- * TODO: `render` currently ignores collapsed trees.
- * TODO: `render` should use d3-flextree, to support varying node sizes.
+ * TODO: Maybe have the argument be a plain div, and not a d3 selection. That
+ *       way any ol tool could be used.
  *
  * @param {Tree} tree Tree to be rendered into SVG.
- * @param {Selection} div `<div>` to be associated with `tree`.
+ * @param {Selection} div `<div>` in which to append the Tree SVG.
  */
 export function render(tree: Tree, div: Div) {
-  // Node Size is determined exclusively by the text contained within the node.
-  // We will treat 1 = 1em in our coordinate system. So the height is the number
-  // of lines of information, while the length the length of the longest line.
-  // This will also use a base padding, defined in `config`.
-  // TODO: I actually want to MAKE the SVGs BEFORE I layout the data, that way I
-  // can calculate true width/height of the text elements.
-  const layout = flextree<TreeData>()
-    .spacing(0)
-    .nodeSize((d) => {
-      return [
-        d.data.size[0] + 2 * (config.padding.x + config.margin.x),
-        d.data.size[1] + 2 * (config.padding.y + config.margin.y),
-      ];
-    });
-  const root = layout.hierarchy(tree as TreeData) as Hierarchy;
+  // TODO: Eventually these will be merged with a config argument
+  const spacing = defaultSpacing;
+  const style = defaultStyle;
 
-  div.html('');
+  const geom = {
+    /**
+     * Bounding Box after applying margin to `pBox`. This should follow nodeSize.
+     */
+    mBox: {
+      x1: (n: Hierarchy) => n.left,
+      y1: (n: Hierarchy) => n.top,
+      x2: (n: Hierarchy) => n.right,
+      y2: (n: Hierarchy) => n.bottom,
+      width: (n: Hierarchy) => geom.mBox.x2(n) - geom.mBox.x1(n),
+      height: (n: Hierarchy) => geom.mBox.y2(n) - geom.mBox.y1(n),
+    },
+
+    /**
+     * Bounding Box after applying padding to the content, `cBox`. This is the
+     * nodeSize with the margin removed.
+     */
+    pBox: {
+      x1: (n: Hierarchy) => n.left + spacing.margin.x,
+      y1: (n: Hierarchy) => n.top + spacing.margin.y,
+      x2: (n: Hierarchy) => n.right - spacing.margin.x,
+      y2: (n: Hierarchy) => n.bottom - spacing.margin.y,
+      width: (n: Hierarchy) => geom.pBox.x2(n) - geom.pBox.x1(n),
+      height: (n: Hierarchy) => geom.pBox.y2(n) - geom.pBox.y1(n),
+    },
+
+    /**
+     * Bounding Box for the node content, before padding and margin.
+     */
+    cBox: {
+      x1: (n: Hierarchy) => n.left + spacing.margin.x + spacing.padding.x,
+      y1: (n: Hierarchy) => n.top + spacing.margin.y + spacing.padding.y,
+      x2: (n: Hierarchy) => n.right - spacing.margin.x - spacing.padding.x,
+      y2: (n: Hierarchy) => n.bottom - spacing.margin.y - spacing.padding.x,
+      width: (n: Hierarchy) => geom.pBox.x2(n) - geom.pBox.x1(n),
+      height: (n: Hierarchy) => geom.pBox.y2(n) - geom.pBox.y1(n),
+    },
+
+    centerX: (n: Hierarchy) => n.x,
+
+    calcApplySpacing: (n: Hierarchy): [number, number] => [
+      n.data.size[0] + 2 * (spacing.padding.x + spacing.margin.x),
+      n.data.size[1] + 2 * (spacing.padding.y + spacing.margin.y),
+    ],
+  };
+
+  const layout = flextree<TreeWithSize>()
+    .spacing(0)
+    .nodeSize(geom.calcApplySpacing);
+  const root = layout.hierarchy(tree as TreeWithSize) as Hierarchy;
   const svg = makeSVG(div);
 
-  initNodes(svg, root);
+  renderNodes(svg, root); // Pre-renders nodes, to calculate size
   layout(root);
-
   sizeSvg(svg, root);
-  makeNodes(svg, root);
-  makeLinks(svg, root);
-  styleTree(svg);
-}
+  renderNodes(svg, root); // Re-renders nodes, once layout is calculated
+  renderLinks(svg, root);
 
-function initNodes(svg: SVG, root: Hierarchy) {
-  svg
-    .select('g.nodes')
-    .selectAll('g.node')
-    .data(root.descendants())
-    .join((enter) => {
-      const node = enter.append('g').classed('node', true);
+  function renderNodes(svg: SVG, root: Hierarchy) {
+    svg
+      .select('g.nodes')
+      .selectAll('g.node')
+      .data(root.descendants())
+      .join(
+        (enter) => {
+          const node = enter.append('g').classed('node', true);
 
-      // Each node attaches its own group
-      node.each((d, i, g) => {
-        d.data.nodeG = select(g[i]).node()!;
+          node
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', 0)
+            .attr('height', 0)
+            .style('fill', 'white')
+            .style('stroke-width', style.strokeWidth)
+            .style('stroke', 'black')
+            .style('opacity', 0.5);
+
+          const nodeType = node
+            .filter((d) => d.data.nodeType != null)
+            .append('text')
+            .attr('x', 0)
+            .attr('y', 0)
+            .classed('nodeType', true)
+            .text((d) => d.data.nodeType?.name ?? '')
+            .attr('font-size', 1)
+            .attr('dominant-baseline', 'hanging')
+            .attr('text-anchor', 'middle');
+          nodeType
+            .filter((d) => d.data.nodeType?.sub != '')
+            .append('tspan')
+            .text((d) => d.data.nodeType!.sub)
+            .attr('font-size', 0.9)
+            .attr('baseline-shift', 'sub');
+          nodeType
+            .filter((d) => d.data.nodeType?.sup != '')
+            .append('tspan')
+            .text((d) => d.data.nodeType!.sup)
+            .attr('font-size', 0.9)
+            .attr('baseline-shift', 'super');
+
+          node
+            .filter((d) => d.data.leaf?.data != null)
+            .append('text')
+            .attr('x', 0)
+            .attr('y', 0)
+            .classed('nodeData', true)
+            .text((d) => d.data.leaf!.data)
+            // Implicitly assuming there can be no data without a type...
+            .attr('dx', 0)
+            .attr('dy', (d) => {
+              // TODO: Use the BBox up to this point to determine spacing
+              return (d.data.nodeType != null ? spacing.lineSpacing : 0) + 'em';
+            })
+            .attr('font-size', 0.9)
+            .attr('dominant-baseline', 'hanging')
+            .attr('text-anchor', 'middle');
+
+          node.each((d, i, n) => {
+            const bb = n[i].getBBox();
+            d.data.size = [bb.width, bb.height];
+          });
+
+          return node;
+        },
+        (update) => {
+          update
+            .select('rect')
+            .attr('x', geom.mBox.x1)
+            .attr('y', geom.mBox.y1)
+            .attr('width', geom.mBox.width)
+            .attr('height', geom.mBox.height);
+
+          update
+            .select('.nodeType')
+            .attr('x', geom.centerX)
+            .attr('y', geom.cBox.y1);
+
+          update
+            .select('.nodeData')
+            .attr('x', geom.centerX)
+            .attr('y', geom.cBox.y1);
+          return update;
+        }
+      );
+  }
+
+  /**
+   * Initializes an SVG object inside the given div. This SVG will only have two
+   * child nodes: `<g class="links">` and `<g class="nodes">`.
+   *
+   * Because rendering takes place inside a `viewBox`, a reasonable max-width is
+   * provided, based on `config.maxScale`.
+   *
+   * @param div parent div element.
+   * @param root data to be rendered in SVG. Provided to generate `viewBox`.
+   * @return newly created SVG child of `div`.
+   */
+  function makeSVG(div: Div) {
+    const svg = div
+      .append('svg')
+      .style('display', 'block')
+      .style('margin', 'auto');
+    svg.append('g').classed('links', true);
+    svg.append('g').classed('nodes', true);
+
+    return svg;
+  }
+
+  function sizeSvg(svg: SVG, root: Hierarchy) {
+    const vb = calcViewBox(root);
+    svg.attr('viewBox', vb.join(' '));
+
+    const parentEm = div.style('font-size').match(/([0-9]*)px/);
+    if (parentEm) {
+      svg.style('max-width', vb[2] * parseInt(parentEm[0]));
+    }
+  }
+
+  function calcViewBox(root: Hierarchy) {
+    const max = { x: -Infinity, y: -Infinity };
+    const min = { x: Infinity, y: Infinity };
+    root.each((n) => {
+      max.x = Math.max(geom.mBox.x2(n), max.x);
+      max.y = Math.max(geom.mBox.y2(n), max.y);
+      min.x = Math.min(geom.mBox.x1(n), min.x);
+      min.y = Math.min(geom.mBox.y1(n), min.y);
+    });
+
+    return [min.x, min.y, max.x - min.x, max.y - min.y];
+  }
+
+  /**
+   * Attach all edges from `root` to `svg`'s `<g class="links">` child.
+   */
+  function renderLinks(svg: SVG, root: Hierarchy) {
+    const enter = svg
+      .select('g.links')
+      .selectAll('.link')
+      .data(root.links())
+      .enter();
+
+    enter
+      .filter((d) => !(d.target.data.leaf?.isCollapsed ?? false))
+      .append('line')
+      .classed('link', true)
+      // For the link, we ignore the y padding
+      .attr('x1', (d) => geom.centerX(d.source as Hierarchy))
+      .attr('y1', (d) => geom.pBox.y2(d.source as Hierarchy))
+      .attr('x2', (d) => geom.centerX(d.target as Hierarchy))
+      .attr('y2', (d) => geom.pBox.y1(d.target as Hierarchy));
+
+    enter
+      .filter((d) => d.target.data.leaf?.isCollapsed ?? false)
+      .append('polygon')
+      .classed('link', true)
+      .attr('points', (d) => {
+        const source = {
+          x: geom.centerX(d.source as Hierarchy),
+          y: geom.pBox.y2(d.source as Hierarchy),
+        };
+        const targ = {
+          y: geom.pBox.y1(d.target as Hierarchy),
+          left: geom.pBox.x1(d.target as Hierarchy),
+          right: geom.pBox.x2(d.target as Hierarchy),
+        };
+        return `${source.x},${source.y} ${targ.left},${targ.y} ${targ.right},${targ.y}`;
       });
 
-      node
-        .append('rect')
-        .attr('x', 0)
-        .attr('y', 0)
-        .attr('width', 0)
-        .attr('height', 0);
-
-      const nodeType = node
-        .filter((d) => d.data.nodeType != null)
-        .append('text')
-        .attr('x', 0)
-        .attr('y', 0)
-        .classed('nodeType', true)
-        .text((d) => d.data.nodeType?.name ?? '')
-        .attr('font-size', 1)
-        .attr('dominant-baseline', 'middle')
-        .attr('text-anchor', 'middle');
-      nodeType
-        .filter((d) => d.data.nodeType?.sub != '')
-        .append('tspan')
-        .text((d) => d.data.nodeType!.sub)
-        .attr('font-size', 0.9)
-        .attr('baseline-shift', 'sub');
-      nodeType
-        .filter((d) => d.data.nodeType?.sup != '')
-        .append('tspan')
-        .text((d) => d.data.nodeType!.sup)
-        .attr('font-size', 0.9)
-        .attr('baseline-shift', 'super');
-
-      node
-        .filter((d) => d.data.leaf?.data != null)
-        .append('text')
-        .attr('x', 0)
-        .attr('y', 0)
-        .classed('nodeData', true)
-        .text((d) => d.data.leaf!.data)
-        // Implicitly assuming there can be no data without a type...
-        .attr('dx', 0)
-        .attr('dy', (d) => {
-          // TODO: Use the BBox up to this point to determine spacing
-          return (d.data.nodeType != null ? config.lineSpacing : 0) + 'em';
-        })
-        .attr('font-size', 0.9)
-        .attr('dominant-baseline', 'middle')
-        .attr('text-anchor', 'middle');
-
-      node.each((d, i, g) => {
-        const bb = d.data.nodeG.getBBox();
-        d.data.size = [bb.width, bb.height];
-      });
-
-      return node;
-    });
-}
-
-/**
- * Initializes an SVG object inside the given div. This SVG will only have two
- * child nodes: `<g class="links">` and `<g class="nodes">`.
- *
- * Because rendering takes place inside a `viewBox`, a reasonable max-width is
- * provided, based on `config.maxScale`.
- *
- * @param div parent div element.
- * @param root data to be rendered in SVG. Provided to generate `viewBox`.
- * @return newly created SVG child of `div`.
- */
-function makeSVG(div: Div) {
-  const svg = div
-    .append('svg')
-    .style('display', 'block')
-    .style('margin', 'auto');
-  svg.append('g').classed('links', true);
-  svg.append('g').classed('nodes', true);
-
-  return svg;
-}
-
-function sizeSvg(svg: SVG, root: Hierarchy) {
-  const vb = viewBox(root);
-  svg.attr('viewBox', vb.join(' ')).style('max-width', vb[2] * config.maxScale);
-}
-
-/**
- * Returns the needed viewBox dimensions of an SVG which should contain `root`,
- * providing a margin based on `config.margin`.
- *
- * @param root data being rendered.
- * @return calculates necessary `[minX, minY, maxX, maxY]`
- */
-function viewBox(root: Hierarchy) {
-  const max = { x: -Infinity, y: -Infinity };
-  const min = { x: Infinity, y: Infinity };
-  root.each((d) => {
-    max.x = Math.max(right(d), max.x);
-    max.y = Math.max(bottom(d), max.y);
-    min.x = Math.min(left(d), min.x);
-    min.y = Math.min(top(d), min.y);
-  });
-
-  return [
-    min.x - config.treeMargin.x,
-    min.y - config.treeMargin.y,
-    max.x - min.x + 2 * config.treeMargin.x,
-    max.y - min.y + 2 * config.treeMargin.y,
-  ];
-}
-
-/**
- * Attach all nodes from `root` to `svg`'s `<g class="nodes">` child.
- */
-function makeNodes(svg: SVG, root: Hierarchy) {
-  const textStartY = (d: Hierarchy) =>
-    top(d) + 0.5 + config.padding.y + config.margin.y;
-
-  const node = svg
-    .select('g.nodes')
-    .selectAll('g.node')
-    .data(root.descendants());
-
-  node
-    .select('rect')
-    .attr('x', (d) => left(d))
-    .attr('y', (d) => top(d))
-    .attr('width', (d) => d.xSize)
-    .attr('height', (d) => d.ySize);
-
-  node.select('.nodeType').attr('x', cx).attr('y', textStartY);
-
-  node.select('.nodeData').attr('x', cx).attr('y', textStartY);
-}
-
-/**
- * Attach all edges from `root` to `svg`'s `<g class="links">` child.
- */
-function makeLinks(svg: SVG, root: Hierarchy) {
-  const enter = svg
-    .select('g.links')
-    .selectAll('line.link')
-    .data(root.links())
-    .enter();
-
-  enter
-    .filter((d) => !(d.target.data.leaf?.isCollapsed ?? false))
-    .append('line')
-    .classed('link', true)
-    // For the link, we ignore the y padding
-    .attr('x1', (d) => cx(d.source as Hierarchy))
-    .attr('y1', (d) => bottom(d.source as Hierarchy) - config.margin.y)
-    .attr('x2', (d) => cx(d.target as Hierarchy))
-    .attr('y2', (d) => top(d.target as Hierarchy) + config.margin.y);
-
-  enter
-    .filter((d) => d.target.data.leaf?.isCollapsed ?? false)
-    .append('polygon')
-    .classed('link', true)
-    .attr('points', (d) => {
-      const source = {
-        x: cx(d.source as Hierarchy),
-        y: bottom(d.source as Hierarchy) - config.margin.y,
-      };
-      const targ = {
-        y: top(d.target as Hierarchy) + config.margin.y,
-        left: left(d.target as Hierarchy) + config.margin.x + config.padding.x,
-        right:
-          right(d.target as Hierarchy) - config.margin.x - config.padding.x,
-      };
-      return `${source.x},${source.y} ${targ.left},${targ.y} ${targ.right},${targ.y}`;
-    });
-}
-
-/**
- * Updates tree with any needed styling.
- *
- * TODO this style should eventually come from `config`
- */
-function styleTree(svg: SVG) {
-  svg
-    .selectAll('g.nodes g.node rect')
-    .style('fill', 'white')
-    .style('stroke-width', config.strokeWidth)
-    .style('stroke', 'black')
-    .style('opacity', 0);
-  svg
-    .selectAll('g.links')
-    .style('stroke', 'black')
-    .style('stroke-width', config.strokeWidth)
-    .style('fill-opacity', 0);
+    svg
+      .select('g.links')
+      .style('stroke', 'black')
+      .style('stroke-width', style.strokeWidth)
+      .style('fill-opacity', 0);
+  }
 }
